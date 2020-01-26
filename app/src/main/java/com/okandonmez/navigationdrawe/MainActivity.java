49331b76bf6fe72baeea4ae12ba40e6e617f93a1
okandonmez <ce.okandonmez@gmail.co>
2020-01-26 19:19:16 +0300

    Layout connected to supportbar

--- app/src/main/java/com/okandonmez/navigationdrawe/MainActivity.java
+++ app/src/main/java/com/okandonmez/navigationdrawe/MainActivity.java
@@ -1,14 +1,40 @@
 package com.okandonmez.navigationdrawe;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = findViewById(R.id.navigation_drawer_layout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
